598bdaafc5a4055060f89946d56b6f5ad76e8c4a
bules <jesus20.11@hotmail.es>
2021-02-03 22:50:56 +0100

    fixed: mainjs

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-window.onload = function() {
-    $(".toggle1").click(function () {
-        $(".panel1").fadeToggle();
-        $(".panel2").fadeOut();
-      });
-      $(".toggle2").click(function () {
-        $(".panel2").fadeToggle();
-        $(".panel1").fadeOut();
-      });
-  }
+window.onload = function () {
+  $(".toggle1").click(function () {
+    $(".panel1").fadeToggle();
+    $(".panel2").fadeOut();
+  });
+  $(".toggle2").click(function () {
+    $(".panel2").fadeToggle();
+    $(".panel1").fadeOut();
+  });
+};
